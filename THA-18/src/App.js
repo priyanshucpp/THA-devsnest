78af1ec341406585dc3a113c0384c9c23878a53f
Priyanshu <priyanshushri7@gmail.com>
2021-07-12 20:44:43 +0530

    Inline Styling

---
+++ THA-18/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./App.css"
+import Chess from "./components/Chess";
+function App(){
+    return(
+        <div>
+<Chess />
+        </div>
+    )
+}
+export default App;
