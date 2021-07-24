b483151f748afbc2d9136028f3126389e191f5db
Priyanshu <priyanshushri7@gmail.com>
2021-07-25 00:53:47 +0530

    Meme generator

---
+++ THA-22/src/App(Prev.).jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [calorie, setCalorie] = useState("");
+  const [cards, setCards] = useState([]);
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    cards.push({ title, calorie, index: cards.length });
+
+    setCards(cards);
+
+    setTitle("");
+    setCalorie("");
+  }
+
+  function handleDelete(index) {
+    setCards(
+      cards.filter((card) => {
+        console.log(index, card.index, card.index !== index);
+        return card.index !== index;
+      })
+    );
+  }
+  return (
+    <>
+      <form className="form" onSubmit={handleClick}>
+        <div className="wrapper">
+          <div className="inputs">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Item Name"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+              type="number"
+              //   type="text"
+              name="calorie"
+              id="calorie"
+              placeholder="Calorie Amount"
+              value={calorie}
+              onChange={(e) => setCalorie(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="submit">
+            ADD ITEM
+          </button>
+        </div>
+      </form>
+      <div className="items">
+        {cards.map((data) => (
+          <Card
+            key={data.index}
+            title={data.title}
+            calorie={data.calorie}
+            handleDelete={() => handleDelete(data.index)}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
